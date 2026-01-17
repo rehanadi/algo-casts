5c3ff15cc05e1affe7f1f5beed72e5052b20c082
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2026-01-17 11:02:50 +0700

    feat: fibonacci + memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,65 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
+
+/*
+function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 1; i <= n; i++) {
+    result.push(result[i] + result[i - 1]);
+  }
+
+  return result[n];
+}
+*/
+
+/*
+// interative solution
+function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+
+  return result[n];
+}
+*/
+
+/*
+// exponential time using recursive (avoid)
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+*/
